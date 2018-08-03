a0d0e0d53246ca001ab7567f2eb61d8add4151e6
SvitlanaAmbros <svitlana.ambros@gmail.com>
2018-08-03 23:35:33 +0300

    add router link for button and img

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-login',
@@ -6,14 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  user:User = {
+  	login:'',
+  	password:''
+  };
 
   constructor() { }
 
   ngOnInit() {
-  	
+
   }
 
   onSubmit() {
-
+  	console.log(this.user.login + this.user.password);
   }
 }
